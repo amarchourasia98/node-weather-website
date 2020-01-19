83ff5197b037513049a73632b3a6fee230880c68
amar <amarkumarchourasia32@gmail.com>
2020-01-19 18:26:10 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const url ='https://api.darksky.net/forecast/c947470c48834cf5f8fd8eeb416923b9/' 
         }else if(body.error){
              callback('unable to find location',undefined)
         }else{
-             callback(undefined,body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degree out. There is a '+ body.currently.precipProbability +' % chance of rain.')
+             console.log(body.daily.data[0])
+             callback(undefined,body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degree out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a '+ body.currently.precipProbability +' % chance of rain.')
         }
     })
 
